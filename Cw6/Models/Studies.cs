936ee151419cbe88994a3198df45e6bf248f4bab
Paweł <73314096+s18277@users.noreply.github.com>
2020-12-05 13:56:58 +0100

    Prepare repo for cw7

--- Cw6/Models/Studies.cs
+++
@@ -1,8 +0,0 @@
-namespace Cw6.Models
-{
-    public class Studies
-    {
-        public int IdStudy { get; set; }
-        public string Name { get; set; }
-    }
-}
---
+++ Cw7/DTOs/Requests/PromoteStudentsRequest.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Cw7.DTOs.Requests
+{
+    public class PromoteStudentsRequest
+    {
+        [Required(ErrorMessage = "Nazwa kierunku jest wymagana do promocji studentów!")]
+        [MaxLength(100)]
+        public string Studies { get; set; }
+
+        [Required(ErrorMessage = "Numer semestru jest wymagany do promocji studentów!")]
+        public int Semester { get; set; }
+    }
+}
