f6ad8eda2349db2ff7cd635ab5321a4680219d2d
Paweł <73314096+s18277@users.noreply.github.com>
2021-01-10 22:35:58 +0100

    Add implementation of EnrollStudent to EntityFrameworkCoreDbStudentService

--- Cw10/Services/DatabaseServices/EntityFrameworkCoreDbStudentService.cs
+++ Cw10/Services/DatabaseServices/EntityFrameworkCoreDbStudentService.cs
@@ -5,6 +5,7 @@ using Cw10.DTOs.Requests;
 using Cw10.DTOs.Responses;
 using Cw10.DTOs.ResultContainers;
 using Cw10.Models.StudentsDatabase;
+using Cw10.Services.EncryptionServices;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cw10.Services.DatabaseServices
@@ -12,10 +13,12 @@ namespace Cw10.Services.DatabaseServices
     public class EntityFrameworkCoreDbStudentService : IDbStudentService
     {
         private readonly StudentsDbContext _dbContext;
+        private readonly IEncryptionService _encryptionService;
 
-        public EntityFrameworkCoreDbStudentService(StudentsDbContext dbContext)
+        public EntityFrameworkCoreDbStudentService(StudentsDbContext dbContext, IEncryptionService encryptionService)
         {
             _dbContext = dbContext;
+            _encryptionService = encryptionService;
         }
 
         public IEnumerable<GetAllStudentsStudentResponse> GetAllStudents()
@@ -50,7 +53,41 @@ namespace Cw10.Services.DatabaseServices
 
         public EnrollmentResult EnrollStudent(EnrollStudentRequest newStudent)
         {
-            throw new NotImplementedException();
+            var studies = _dbContext.Studies.FirstOrDefault(s => s.Name == newStudent.Studies);
+            if (studies == null)
+                return new EnrollmentResult {Error = $"Kierunek {newStudent.Studies} nie istnieje!"};
+            if (_dbContext.Students.Any(s => s.IndexNumber == newStudent.IndexNumber))
+                return new EnrollmentResult {Error = $"Numer indeksu {newStudent.IndexNumber} już istnieje!"};
+            var enrollment = GetEnrollmentForNewStudent(studies);
+            var student = new Student
+            {
+                IndexNumber = newStudent.IndexNumber,
+                FirstName = newStudent.FirstName,
+                LastName = newStudent.LastName,
+                BirthDate = newStudent.BirthDate,
+                SaltPasswordHash = _encryptionService.Encrypt(newStudent.Password)
+            };
+            enrollment.Students.Add(student);
+            _dbContext.Students.Add(student);
+            if (_dbContext.SaveChanges() == 0)
+                return new EnrollmentResult
+                {
+                    Successful = false,
+                    Error = "Nie zmodyfikowano żadnych wierszy w bazie danych!"
+                };
+            return new EnrollmentResult
+            {
+                Successful = true,
+                Student = student,
+                Enrollment = new EnrollmentDto
+                {
+                    IdEnrollment = enrollment.IdEnrollment,
+                    Semester = enrollment.Semester,
+                    IdStudy = enrollment.IdStudy,
+                    StudiesName = studies.Name,
+                    StartDate = enrollment.StartDate
+                }
+            };
         }
 
         public EnrollmentDto PromoteStudents(PromoteStudentsRequest promoteStudentsRequest)
@@ -96,8 +133,24 @@ namespace Cw10.Services.DatabaseServices
         public int DeleteStudent(string indexNumber)
         {
             var student = _dbContext.Students.First(s => s.IndexNumber == indexNumber);
-            _dbContext.Students.Remove(student);
+            _dbContext.Remove(student);
             return _dbContext.SaveChanges();
+        }
+
+        private Enrollment GetEnrollmentForNewStudent(Study studies)
+        {
+            var enrollment = _dbContext.Enrollments
+                .Where(e => e.Semester == 1)
+                .FirstOrDefault(e => e.IdStudy == studies.IdStudy);
+            if (enrollment != null) return enrollment;
+            var newEnrollment = new Enrollment
+            {
+                Semester = 1,
+                IdStudy = studies.IdStudy,
+                StartDate = DateTime.Now
+            };
+            _dbContext.Enrollments.Add(newEnrollment);
+            return newEnrollment;
         }
     }
 }
